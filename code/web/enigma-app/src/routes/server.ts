20c96fdec970fcd252431e53f76ba645f40eba85
HVerschueren <r0842391@student.thomasmore.be>
2023-01-17 17:36:45 +0100

    test

--- code/web/enigma-app/src/routes/server.ts
+++
@@ -1,7 +0,0 @@
-import { error, json, type RequestHandler } from '@sveltejs/kit';
-import status from 'http-status';
-
-const messageFromEnigma = platform.env.MESSAGE;
-const messageP = document.getElementById("message");
-messageP?.textContent = messageP;
-

b271d515b1ec6a3de394f6def11103319ff4be3d
HVerschueren <r0842391@student.thomasmore.be>
2023-01-17 16:04:44 +0100

    declare toegevoegd

--- code/web/enigma-app/src/app.d.ts
+++ code/web/enigma-app/src/app.d.ts
@@ -2,20 +2,20 @@
 // for information about these interfaces
 // and what to do when importing types
 
-namespace App {
+declare namespace App {
 // interface Error {}
 // interface Locals {}
 // interface PageData {}
 // interface Platform {}
-interface Platform {
-	env: {
-		MESSAGE: KVNamespace;
-		API_TOKEN: String;
-	};
-	context: {
-		waitUntil(promise: Promise<any>): void;
-	};
-	caches: CacheStorage & { default: Cache }
-}
+	interface Platform {
+		env: {
+			MESSAGE: KVNamespace;
+			API_TOKEN: String;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
 }
 

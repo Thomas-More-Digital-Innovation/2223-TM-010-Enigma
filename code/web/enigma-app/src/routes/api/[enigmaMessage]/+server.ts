99385de44c497e3656319b61e47047f87a8dadc4
HVerschueren <r0842391@student.thomasmore.be>
2023-01-17 16:56:16 +0100

    proberen message op website te krijgen

--- code/web/enigma-app/src/routes/api/[enigmaMessage]/+server.ts
+++ code/web/enigma-app/src/routes/api/[enigmaMessage]/+server.ts
@@ -3,16 +3,19 @@ import status from 'http-status';
 
 export const POST: RequestHandler = async ({ params, request, platform }) => {
 	const messageFromEnigma = params.enigmaMessage;//haalt message uit url
+  var messageP = document.getElementById('message');
+  messageP.textContent = "test";
+  console.log(messageFromEnigma);
+  console.log(typeof messageFromEnigma);
+  //checkt of er een api key is
+  if (!request.headers.has('x-api-key')) throw error(status.BAD_REQUEST, 'Missing X-API-Key'); 
+  //checkt of api key juist is 
+  if (request.headers.get('x-api-key') !== platform.env.API_TOKEN)
+  throw error(status.UNAUTHORIZED, 'Invalid API key');
 
-    //checkt of er een api key is
-    if (!request.headers.has('x-api-key')) throw error(status.BAD_REQUEST, 'Missing X-API-Key'); 
-    //checkt of api key juist is 
-    if (request.headers.get('x-api-key') !== platform.env.API_TOKEN)
-		throw error(status.UNAUTHORIZED, 'Invalid API key');
+  await platform.env.MESSAGE.put("enigmaMessage", messageFromEnigma);
 
-    await platform.env.MESSAGE.put("enigmaMessage", messageFromEnigma);
-
-    return json(
-        { status: status.CREATED }
-    );
+  return json(
+      { status: status.CREATED }
+  );
 }

e3cc0133b32c4e581ec9996d64a5d4618664495c
HVerschueren <r0842391@student.thomasmore.be>
2023-01-17 14:58:52 +0100

    begin api geschreven

--- code/web/enigma-app/src/app.d.ts
+++ code/web/enigma-app/src/app.d.ts
@@ -7,6 +7,16 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		interface Platform {
+		env: {
+			MESSAGE: KVNamespace;
+			API_TOKEN: String;
+			};
+			context: {
+			waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+			}
 	}
 }
 
